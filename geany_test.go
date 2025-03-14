e98ccf296fc0b910fc72d2eed39df35b99faf9a7
Alexander Adam <alphaone23@gmail.com>
2025-03-14 02:15:53 +0100

    Added logo printing test.

--- geany_test.go
+++ geany_test.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
+	"io"
+	"os"
 	"runtime/debug"
 	"testing"
 
@@ -20,4 +23,25 @@ func TestPrepareLogoData(t *testing.T) {
 	assert.Equal(t, logoData.Geany.VcsTime, "unknown", "Geany.VcsTime is not unknown")
 	assert.Equal(t, logoData.Geany.VcsRevision, "unknown", "Geany.VcsRevision is not unknown")
 	assert.Equal(t, logoData.Geany.GoVersion, buildInfo.GoVersion, fmt.Sprintf("Geany.GoVersion is not %s", buildInfo.GoVersion))
+}
+
+func TestPrintLogoWriter(t *testing.T) {
+	logoTemplateFile, logoTemplateFileErr := os.Open("testData/logo.tpl")
+
+	assert.Nil(t, logoTemplateFileErr, "could not open template file")
+
+	defer func() { _ = logoTemplateFile.Close() }()
+
+	logoTemplate, readErr := io.ReadAll(logoTemplateFile)
+
+	assert.Nil(t, readErr, "could not read logo template")
+
+	target := bytes.Buffer{}
+
+	assert.Nil(t, PrintLogoWriter(&target, string(logoTemplate), nil), "logo printing error")
+
+	buildInfo, ok := debug.ReadBuildInfo()
+
+	assert.True(t, ok, "build info not found in debug.ReadBuildInfo")
+	assert.Contains(t, target.String(), buildInfo.GoVersion, "Logo does not contain go version")
 }
